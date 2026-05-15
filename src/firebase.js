39b513763488d4b8def4edb95d453bda58cfa262
Shiwank <shashigupta03021@gmail.com>
2026-05-15 08:58:13 +0530

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDI702IvJeWC8gwUqPwHbnKmjQUy7Uayok",
+  apiKey: "AIzaSyAxGM8uwA8VVTUqbUaA3c1QLchMOj4Ckwg",
   authDomain: "smart-coach-d7951.firebaseapp.com",
   projectId: "smart-coach-d7951",
   storageBucket: "smart-coach-d7951.firebasestorage.app",
